394cdb95554c7b921d3d9b0e6198430f6dc51834
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2022-09-03 17:21:48 -0400

    Completed Report

--- REPORT.md
+++ REPORT.md
@@ -9,22 +9,24 @@ Your tone should be professional but informal: imagine that you are responding t
 ---
 
 1. Who was the top salesperson for the year?
-
+Abraham Bennett
 
 2. How much better did the top salesperson do than the individual who came in second for the year?
-
+Abraham Bennet is the top sales person with 118 sales. Michael O'Leary came in second place with 70 sales.
 
 3. Was there any relationship between the average sale amount for the top salespersons and the total sales?
-
+ 
 
 4. How do you think the top selling salesperson gained so many sales? What was their strategy?
 
 - Did they sell to many customers, or a dedicated few?
 - Did they tend to sell a few big ticket (high priced) items, or did they focus on a high volume of lower priced items?
-
+Abraham Bennett, the top salesperson, also sold to the customer with the repeat purchases; CustomerID 17664. Abraham did not focus on any specific products volume or price point.
  
 
 5. What was the top selling product for the year? What was the lowest selling? (For both dollar amount sold and quantity sold).
-
+Road-350-W Yellow, 42 was the top selling product with 983 units sold. Totaling $1,672,073.17 in sales.
+However, the highest priced product, Mountain-100 Silver, 38 sold 780 units totaling $2,651,992.20.
 
 6. What problems did you find in the data? (For example, where there any products without a price?)
+Yes, there are 5 products with a price listed as 0. These products consist of bolts and washers that are likely purchased along with a larger product with a higher price point.
--- salesperson of the year.sql
+++ salesperson of the year.sql
@@ -1,9 +1,52 @@
+-- Problem 1 & 2
+-- 1. Using JOINs in a single query, combine data from all three tables (employees, products, sales) to view all sales with complete employee and product information in one table.
+CREATE VIEW employee_sales AS SELECT * FROM employees LEFT JOIN sales ON employees.EmployeeID= sales.SalesPersonID;
+-- 2a. Create a View for the query you made in Problem 1 named "all_sales"
+-- NOTE: You'll want to remove any duplicate columns to clean up your view!
+ 
+ALTER TABLE products RENAME COLUMN ProductID TO Product_ID;
+-- error when attempting to join. different number of columns
+CREATE VIEW all_sales AS SELECT * FROM employee_sales LEFT JOIN products ON employee_sales.ProductID=products.Product_ID;
 
--- SELECT name, price FROM all_sales ORDER BY price DESC LIMIT 1;
--- SELECT firstName, LastName, COUNT(*) as total_sales_by_employee FROM all_sales GROUP BY FirstName ORDER BY COUNT(FirstName) DESC LIMIT 3;
--- SELECT name, COUNT(*) as product_sold_most FROM all_sales GROUP BY name ORDER BY COUNT(name) DESC LIMIT 3;
--- SELECT avg(price) FROM all_sales;
--- SELECT name, price FROM all_sales WHERE price IN (SELECT price FROM all_sales WHERE price > 528.21) ORDER BY price;
--- SELECT CustomerID, quantity, COUNT(*) as most_frequent_customer FROM all_sales GROUP BY CustomerID ORDER BY COUNT(CustomerID) DESC, QUANTITY DESC LIMIT 3;
--- SELECT firstName, LastName, COUNT(*) as total_sales_by_employee FROM all_sales GROUP BY FirstName ORDER BY COUNT(FirstName);
+-- 2b. Test your View by selecting all rows and columns from the View
+SELECT * FROM all_sales;
+-- Problem 3
+-- Find the average sale amount for each sales person
+
+-- Problem 4
+-- Find the top three sales persons by total sales
+SELECT firstName, LastName, COUNT(*) as total_sales_by_employee FROM all_sales GROUP BY FirstName ORDER BY COUNT(FirstName) DESC LIMIT 3;
+-- Problem 5
+-- Find the product that has the highest price
+SELECT name, price FROM all_sales ORDER BY price DESC LIMIT 1;
+-- Problem 6
+-- Find the product that was sold the most times
+SELECT name, COUNT(*) as product_sold_most FROM all_sales GROUP BY name ORDER BY COUNT(name) DESC LIMIT 3;
+-- Problem 7
+-- Using a subquery, find all products that have a price higher than the average price for all products
+SELECT avg(price) FROM all_sales; 
+-- 528.21
+SELECT name, price FROM all_sales WHERE price IN (SELECT price FROM all_sales WHERE price > 528.21) ORDER BY price;
+-- Problem 8
+-- Find the customer who spent the most money in purchased products
+SELECT CustomerID, COUNT(*) as most_frequent_customer FROM all_sales GROUP BY CustomerID ORDER BY COUNT(CustomerID) DESC LIMIT 3;
+-- Problem 9
+-- Find the total number of sales for each sales person
+SELECT firstName, LastName, COUNT(*) as total_sales_by_employee FROM all_sales GROUP BY FirstName ORDER BY COUNT(FirstName);
+-- Problem 10
+-- Find the sales person who sold the most to the customer you found in Problem 8
 SELECT CustomerID, quantity, salespersonID, firstname, lastname, COUNT(*) as most_frequent_customer FROM all_sales GROUP BY CustomerID ORDER BY COUNT(CustomerID) DESC, QUANTITY DESC LIMIT 1;
+
+
+SELECT name, price FROM all_sales ORDER BY price DESC LIMIT 1;
+SELECT firstName, LastName, COUNT(*) as total_sales_by_employee FROM all_sales GROUP BY FirstName ORDER BY COUNT(FirstName) DESC LIMIT 3;
+SELECT name, COUNT(*) as product_sold_most FROM all_sales GROUP BY name ORDER BY COUNT(name) DESC LIMIT 3;
+SELECT avg(price) FROM all_sales;
+-- 528.21
+SELECT name, price FROM all_sales WHERE price IN (SELECT price FROM all_sales WHERE price > 528.21) ORDER BY price;
+SELECT CustomerID, quantity, COUNT(*) as most_frequent_customer FROM all_sales GROUP BY CustomerID ORDER BY COUNT(CustomerID) DESC, QUANTITY DESC LIMIT 3;
+SELECT firstName, LastName, COUNT(*) as total_sales_by_employee FROM all_sales GROUP BY FirstName ORDER BY COUNT(FirstName);
+-- SELECT avg(count(*)) FROM all_sales ORDER BY firstname;
+SELECT CustomerID, quantity, salespersonID, firstname, lastname, COUNT(*) as most_frequent_customer FROM all_sales GROUP BY CustomerID ORDER BY COUNT(CustomerID) DESC, QUANTITY DESC LIMIT 1;
+SELECT ProductID, name, quantity, price FROM all_sales GROUP BY name ORDER BY Quantity DESC, price DESC;
+SELECT DISTINCT name, price FROM all_sales WHERE price = 0;
