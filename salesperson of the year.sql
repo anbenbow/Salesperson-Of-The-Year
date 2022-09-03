983014a208b59a2ca3e2feeff31d4246ed25a23f
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2022-09-02 22:59:38 -0400

    Created employee sales view

--- salesperson of the year.sql
+++ salesperson of the year.sql
@@ -1,1 +1,6 @@
-SELECT * FROM sales;
+-- SELECT * FROM employees LEFT JOIN sales ON employees.EmployeeID= sales.SalesPersonID;
+-- UNION
+-- SELECT * FROM sales LEFT JOIN products ON sales.ProductID=products.ProductID;
+-- CREATE VIEW employee_sales AS SELECT * FROM employees LEFT JOIN sales ON employees.EmployeeID= sales.SalesPersonID;
+-- CREATE VIEW product_sales AS SELECT * FROM sales LEFT JOIN products ON sales.ProductID=products.ProductID;
+SELECT * FROM product_sales;
